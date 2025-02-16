74ed72344f9451b651ebb39a5db53297bc1c013b
Fernando <fernandogar9473@gmail.com>
2025-02-15 19:04:54 -0600

    Primer commit - Primer sprint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { primerComponente } from './components/primerComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <primerComponente />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/primerComponente.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const primerComponente = () => {
+  return (
+    <div>
+      <ul>
+          <li> <img src="logo.png" id="logo"/></li>
+          <li> <a href="main.html">Inicio</a> </li>
+          <li> <a>Rutas</a> </li>
+          <li> <a>Avisos</a> </li>
+          <li> <a>Pagos</a> </li>
+          <li> <a>Sugerencias</a> </li>
+          <li> <a href="login.html">Inicio de sesión</a> </li>
+
+        </ul>
+    </div>
+  )
+}
+
